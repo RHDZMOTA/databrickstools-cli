c8759ea3119c4e0a5e683ca83a9304be53f54036
rhdzmota <rhdzmota@gmail.com>
2020-04-05 01:41:37 -0600

    Project config

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md") as file:
+    readme = file.read()
+
+with open("requirements.txt", "r") as file:
+    req = file.read()
+
+setup(
+    name="databrickstools",
+    version="0.1.0",
+    description="A simple commandline application to manage databricks resources.",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author="Rodrigo Hernández Mota",
+    author_email='contact@rhdzmota.com',
+    url="https://github.com/rhdzmota/databrickstools-cli",
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[r for r in req.split("\n") if r],
+    python_requires='>=3.5, <4',
+    entry_points={
+      "console_scripts": [
+          "databrickstools=databrickstools:main"
+      ]
+    },
+    license="LICENSE.md",
+    scripts=["bin/databrickstools"],
+)
